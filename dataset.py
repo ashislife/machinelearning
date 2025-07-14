a34ccb5a611d1e8408db4d6d231e579930db4450
Ashish Mehra <ashismehra095@gmail.com>
2025-07-14 10:43:17 +0530

    push dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import numpy as np
 
-dic={"A":[12,13,45,6,78],"b":[18,53,23,76,98],"c":[28,33,63,6,938]}
+dic={"A":np.random.randint(1,100,size=60),
+     "B":np.random.randint(1,100,size=60),
+     "C":np.random.randint(1,10,size=60),
+     "D":np.random.randint(1,120,size=60),
+     "E":np.random.randint(1,340,size=60),
+     "F":np.random.randint(1,30,size=60),
+     "G":np.random.randint(1,700,size=60),
+     "H":np.random.randint(1,40,size=60),
+     "I":np.random.randint(1,450,size=60),
+     "J":np.random.randint(1,1340,size=60),
+     "K":np.random.randint(1,450,size=60),
+     "L":np.random.randint(1,350,size=60)
 
-d=pd.DataFrame(dic)
-print(d)
-d.to_csv("new_file1.csv")
+
+     }
+
+df=pd.DataFrame(dic)
+print(df)
+df.to_csv("new_file1.csv")
